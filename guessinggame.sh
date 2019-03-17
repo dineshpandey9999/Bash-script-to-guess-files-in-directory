76053fc46791b74f558f1f2d722c9599fdd6215a
dineshpandey9999 <46136120+dineshpandey9999@users.noreply.github.com>
2019-03-17 12:14:41 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+
 files=$(ls|wc -l)
-while true; do
-echo "Guess how many numbers of are in this folder"
-read response
-if test "$files" -eq "$response";then
-echo "Well done"
-break
-else
-echo "Please Try again"
-fi
-done
+
+function guess_game{
+    
+    while true; do
+    echo "Guess how many numbers of are in this folder"
+    read response
+    
+    if test "$files" -eq "$response";then
+    echo "Well done"
+    break
+    
+    elif test "$files" -gt "$response";then
+    echo "Your guess is too low..."
+    
+    elif test "$files" -lt "$response";then
+    echo "Your guess is too high..."
+    fi
+    done
+}
+
+guess_game
